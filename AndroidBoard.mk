c8cbae0a61da5b3c4c1146b66d896e45b03fa210
h2o64 <theh2o64@gmail.com>
2014-09-13 11:08:09 +0200

    Fix the lastest commit

---
+++ AndroidBoard.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call mydir)
+
+# include the nonopensource counterpart to this file
+include vendor/motorola/xt1034/AndroidBoardVendor.mk
+
+#
+# Compile Linux Kernel
+#
+KERNEL_DEFCONFIG := msm8226_mmi_defconfig
+
+include device/qcom/msm8226/AndroidBoard.mk
+
+#Create symbolic links
+$(shell mkdir p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
+        mkdir p $(TARGET_OUT_VENDOR)/lib; \
+        rm $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
+        ln sf /persist/WCNSS_qcom_wlan_factory_nv.bin \
+        $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_factory_nv.bin; \
+        ln sf /system/lib/libQSEEComAPI.so \
+        $(TARGET_OUT_VENDOR)/lib/libQSEEComAPI.so)
