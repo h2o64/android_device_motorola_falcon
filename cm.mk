f27fd795719667381955465d06e2fbf478ae70ea
Louis Popi <theh2o64@gmail.com>
2015-07-30 00:53:57 +0200

    falcon : Set those builds as unofficial CM 	* This is CM-12.1-CAF

--- cm.mk
+++ cm.mk
@@ -3,5 +3,7 @@ $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+TARGET_UNOFFICIAL_BUILD_ID := h2o64
+
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := cm_falcon
