e77d0902c4557020c70ec0fbf597d69b67ec71da
LuK1337 <priv.luk@gmail.com>
2015-10-13 23:26:21 +0200

    falcon: Build init as C++
    Change-Id: I310a0a079caa49be56b7090f75db4a9129d49a80

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,7 +113,7 @@ EXTENDED_FONT_FOOTPRINT := true
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_falcon.c
+TARGET_LIBINIT_DEFINES_FILE := device/motorola/falcon/init/init_falcon.cpp
 TARGET_UNIFIED_DEVICE := true
 
 # Lights
