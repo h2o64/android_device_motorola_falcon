93d6a8d2a44e52b7a5b737e5b3ff6593db874f8d
Konsta <konsta09@gmail.com>
2015-10-20 15:36:10 +0200

    msm8226-common: fm: Update for M
    * This partially reverts commit f9f9231a48185bac5a852b0c6f71548c1ac649fb.
    * This is a combination of following commits:
      https://www.codeaurora.org/cgit/quic/la/device/qcom/common/commit/?h=LA.BF.1.1.3_rb1.2&id=f20579727e984b3812232d6ff5e3d8359d8dfbab
      https://www.codeaurora.org/cgit/quic/la/device/qcom/common/commit/?h=LA.BF.1.1.3_rb1.2&id=6cc2ced119bb7680ad68eb61c00e87c7308c943f
      https://www.codeaurora.org/cgit/quic/la/device/qcom/common/commit/?h=LA.BF.1.1.3_rb1.2&id=b1d54f640c180620427bea58c07a3fb6b647746e
      https://www.codeaurora.org/cgit/quic/la/device/qcom/common/commit/?h=LA.BF.1.1.3_rb1.2&id=811a1b1fe28b57de08c52ec7f13e4d7534b4e906
      https://www.codeaurora.org/cgit/quic/la/platform/vendor/qcom/msm8226/commit/?h=LA.BF.1.1.3_rb1.2&id=e2f1b661e001abf9e4277a1ab88d5e17f22b1390
    
    Change-Id: Idce3605937e648bc52dd41ccb5fbb03ad9e68fc4

--- device.mk
+++ device.mk
@@ -109,10 +109,8 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
+    FMRadio \
+    libfmjni
 
 # GPS
 PRODUCT_PACKAGES += \
@@ -156,6 +154,7 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
+    init.qcom.fm.sh \
     init.qcom.ril.sh \
     mount_pds.sh
 
