9b7ca9e2d4052765e23ec71bcbbcdac8376c94e8
Louis Popi <theh2o64@gmail.com>
2015-10-27 19:53:56 +0100

    falcon: Squashed updates from LA.BF.1.1.3_rb1.3

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,10 @@
 /*
+ *
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
  * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2015 CodeAurora Forums
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,9 +21,7 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
 #define BTM_DEF_LOCAL_NAME   "Moto G"
-#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_L2CAP_CHANNELS    16
@@ -27,4 +29,5 @@
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
 #define BLE_PERIPHERAL_ADV_NAME  FALSE
+#define BT_CLEAN_TURN_ON_DISABLED 1
 #endif
